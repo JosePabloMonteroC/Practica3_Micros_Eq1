d1b88e4efce889384105087cf9bdcfb49a74d96d
Jose Pablo <monterojp02@gmail.com>
2022-11-16 18:43:56 +0000

    Ultimos ajustes

--- main.cpp
+++ main.cpp
@@ -19,7 +19,10 @@ int salidas[17][7] = {{0, 0, 0, 0, 0, 0, 1},    // 0
                       {0, 1, 1, 1, 0, 0, 0},   // 15 (F)
                       {0, 0, 0, 0, 0, 0, 1}};  // 0
 
-DigitalOut myled(LED1);
+DigitalOut ledRojo(LED1);
+DigitalOut ledVerde(LED2);
+DigitalOut ledAzul(LED3);
+
 
 /**Arreglo bidimensional de puertos para los segmentos*/
 DigitalOut segm01[2][7] = {{PTC11, PTC10, PTC6, PTC5, PTC4, PTC3, PTC0},    //Segmento 10
@@ -30,19 +33,23 @@ DigitalIn BtnA(PTB9);  // Boton aumentar velocidad
 DigitalIn BtnB(PTB10); // Boton reducir velocidad
 
 /**Variables globales*/
-long int frecuencia =1000000;           //Frecuencia inicial 1s, minimo 0.3s, máximo 0.6s
+long int vel =1000000;                   //vel inicial 1s, minimo 0.3s, máximo 2s
 int indicador = 0;                      //Variable que definirá si la cuenta es ascendente o descendente.
 int ultimo10 = 0;                       //Variable que almacenará el último número en el que se quedó el segmento más significativo.
 int ultimo01 = 0;                       //Variable que almacenará el último número en el que se quedó el segmento menos significativo.
 
 /**Prototipos de funcion*/
-void imprimir(int i, int j, int frecuencia);
+void imprimir(int i, int j, int vel);
 void up(void);
 void down(void);
 
 /**Función principal*/
 int main() {
   
+  ledRojo = 1;
+  ledVerde = 1;
+  ledAzul = 1;
+
   while(1){
     
     switch(indicador){
@@ -59,18 +66,19 @@ int main() {
   return 0;
 }
 
+
 /**
 @fn imprimir
 @details Funcion que manda un 0 o un 1 a las salidas del segmento correspondiente para encender el número.
-@param variable i para definir cual de los 2 segmentos encender, variable j para indicar que numero encender, frecuencia para controlar la velocidad de espera.
+@param variable i para definir cual de los 2 segmentos encender, variable j para indicar que numero encender, vel para controlar la velocidad de espera.
 @return
 */
-void imprimir(int i, int j, int frecuencia) {
+void imprimir(int i, int j, int vel) {
   for (int k = 0; k <= 6; k++) 
   {
     segm01[i][k] = salidas[j][k];
   }
-  wait_us(frecuencia);
+  wait_us(vel);
 }
 
 /**
@@ -83,30 +91,34 @@ void up(){
     for(int i = ultimo10; i <= 16; i++)
     {
         
-        imprimir(0, i, frecuencia);
+        imprimir(0, i, vel);
 
         for(int j = ultimo01; j <= 16; j++)
         {
-            imprimir(1, j, frecuencia);
+            imprimir(1, j, vel);
             if(BtnUD == 1)
             {
                 ultimo10 = i;
                 ultimo01 = j;
                 indicador = 1;
+                ledAzul= 0;
+                wait_us(100000);
+                ledAzul = 1;
                 return;
             }
-                 myled = 1;
-            if(BtnA == 1 && frecuencia >= 300000)
+            if(BtnA == 1 && vel >= 300000)
             {
-                myled = 0;
-                wait_us(200000);
-                frecuencia -= 300000;
+                ledVerde = 0;
+                wait_us(100000);
+                ledVerde = 1;
+                vel -= 300000;
             }
-            else if(BtnB == 1 && frecuencia <= 2000000)
+            else if(BtnB == 1 && vel <= 2000000)
             {
-                myled = 0;
+                ledRojo = 0;
                 wait_us(200000);
-                frecuencia += 300000;
+                ledRojo = 1;
+                vel += 300000;
             }
             
         }
@@ -125,30 +137,33 @@ void up(){
 void down(){
     for(int i = ultimo10; i >= 0; i--)
     {
-        imprimir(0, i, frecuencia);
+        imprimir(0, i, vel);
         for(int j = ultimo01; j >= 0; j--)
         {
-            imprimir(1, j, frecuencia);
+            imprimir(1, j, vel);
             if(BtnUD == 1)
             {
-                
                 ultimo10 = i;
                 ultimo01 = j;
                 indicador = 0;
+                ledAzul = 0;
+                wait_us(100000);
+                ledAzul = 1;
                 return;
             }
-            myled = 1;
-            if(BtnA == 1 && frecuencia >= 300000)
+            if(BtnA == 1 && vel >= 300000)
             {
-                myled = 0;
+                ledVerde = 0;
                 wait_us(200000);
-                frecuencia -= 300000;
+                ledVerde = 1;
+                vel -= 300000;
             }
-            else if(BtnB == 1 && frecuencia <= 2000000)
+            else if(BtnB == 1 && vel <= 2000000)
             {
-                myled = 0;
+                ledRojo = 0;
                 wait_us(200000);
-                frecuencia += 300000;
+                ledRojo = 1;
+                vel += 300000;
             }
             
         }
